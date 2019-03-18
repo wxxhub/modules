4e5a3c382b3558f8ece2d9d5c1124d120d6d6581
wxx <614610440@qq.com>
2019-03-18 11:01:17 +0800

    end game

--- static/js/index.js
+++ static/js/index.js
@@ -6,7 +6,7 @@ function sp(){
     var tex = $('input_text').value;
     var nun =tex.length;
     if (nun >= 1){
-        $('submit_button').src="";
+        $('submit_button').src="/static/image/send.jpg";
         $('submit_button').disabled="";
     }else{
         $('submit_button').src="/static/image/plus.jpg";
